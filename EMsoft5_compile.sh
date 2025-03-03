eb4eba36b317bced9f840a0435f98c7a31eded18
mechlin <mechlin@hotmail.com>
2025-03-03 13:19:04 -0800

    Update EMsoft5_compile.sh for /opt/pollock dir compile

--- EMsoft5_compile.sh
+++ EMsoft5_compile.sh
@@ -4,22 +4,22 @@ apt-get update -y
 apt-get upgrade -y
 apt-get install build-essential git gcc gfortran cmake ocl-icd-opencl-dev libblas-dev liblapack-dev -y
 
-cd /home/mechlin/Build
+cd /opt/pollock
 
 #EMsoft Superbuild
 git clone https://github.com/EMsoft-org/EMsoftSuperbuild.git -b develop EMsoftSuperbuild5
 cd EMsoftSuperbuild5
 # mkdir Debug
 # cd Debug
-# cmake -DEMsoftOO_SDK=/home/mechlin/Build/EMsoftSuperbuildOO -DCMAKE_BUILD_TYPE=Debug ../
+# cmake -DEMsoftOO_SDK=/opt/pollock/EMsoftSuperbuildOO -DCMAKE_BUILD_TYPE=Debug ../
 # make -j
 # cd ../
 mkdir Release
 cd Release
-cmake -DEMsoft_SDK=/home/mechlin/Build/EMsoftSuperbuild5/ -DCMAKE_BUILD_TYPE=Release ../
+cmake -DEMsoft_SDK=/opt/pollock/EMsoftSuperbuild5/ -DCMAKE_BUILD_TYPE=Release ../
 make -j
 
-cd /home/mechlin/Build 
+cd /opt/pollock 
  
 #EMsoftOO Build
 git clone https://github.com/EMsoft-org/EMsoft.git -b develop EMsoft5
@@ -27,11 +27,11 @@ mkdir EMsoft5Build
 cd EMsoft5Build
 mkdir Release
 cd Release
-cmake -DCMAKE_BUILD_TYPE=Release -DEMsoft_SDK=/home/mechlin/Build/EMsoftSuperbuild5/ ../../EMsoft5
+cmake -DCMAKE_BUILD_TYPE=Release -DEMsoft_SDK=/opt/pollock/EMsoftSuperbuild5/ ../../EMsoft5
 make
 #cd ../
 #mkdir Debug
 #cd Debug
-#cmake -DCMAKE_BUILD_TYPE=Debug -DEMsoftOO_SDK=/home/mechlin/Build/EMsoftSuperbuildOO/ ../../EMsoftOO
+#cmake -DCMAKE_BUILD_TYPE=Debug -DEMsoftOO_SDK=/opt/pollock/Build/EMsoftSuperbuildOO/ ../../EMsoftOO
 #make -j
  
